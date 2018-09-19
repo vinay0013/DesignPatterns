6853d30f27ee951eb2c10e0c4648b1384bf51dc7
VI20009890 <VI20009890@D-113155558.wipro.com>
2018-09-19 18:56:23 +0530

    composite pattern

---
+++ CompositePattern/src/Circle.java
@@ -0,0 +1,10 @@
+
+public class Circle implements Shape {
+
+	@Override
+	public void draw(String fillColor) {
+		// TODO Auto-generated method stub
+		System.out.println("Drawing Circle with color "+fillColor); 
+	}
+
+}
---
+++ CompositePattern/src/Triangle.java
@@ -0,0 +1,10 @@
+
+public class Triangle implements Shape{
+
+	@Override
+	public void draw(String fillColor) {
+		// TODO Auto-generated method stub
+		 System.out.println("Drawing Triangle with color "+fillColor); 
+	}
+
+}
